81a91aaf8aeeeb145de42c2bced7a4d950bf4932
petertenfields-ui <peter.tenfields@gmail.com>
2025-12-04 19:18:48 +0100

    Update meteor.py
    Tkt pas de toutes les variables ça gère la simulation de gravité

--- lib/force_a_toi/meteor.py
+++ lib/force_a_toi/meteor.py
@@ -6,19 +6,47 @@ import math
 with open("meteor_sprite_sheet.txt","r") as f:
     b64=f.read()
 class Meteor:
-    def __init__(self,center,image=b64,frame_index=0):
+    def __init__(self,center,image=b64,frame_index=0,object_density=3000.0):
         self.image=pygame.image.load(BytesIO(base64.b64decode(image))).convert_alpha()
         self.rect=self.image.get_rect()
         self.rect.center=center
-        self.speed=randint(2,10)
+        self.point_of_fall=center
+        self.speed=randint(1,3)
+        self.gravity=0.1
         self.x_speed=randint(2,10) if center[0]==25 else randint(-10,-2) if center[0]==910 else randint(-10,10)
         self.size=randint(1,6)*0.1
+        self.meteor_density=object_density
+        self.air_density=1.225
+        self.Cd=1.0
+        self.mps=0.02 #pour la conversion de metres en pixels
+        self.fps=60.0
+        self.dt=1.0/60.0
+        self.g=9.81
         self.frames=self.load_frames()
+        self.frame=self.frames[0]
         self.frame_index=frame_index
+        self.radius=self.frames[0].get_width()*self.mps/2.0
+        self.surface=math.pi*self.radius**2
+        self.mass=self.meteor_density*(4.0/3.0)*math.pi*(self.radius**3) #on assume que l'objet est une sphère
+        self.term_v=math.sqrt((2*self.mass*self.g)/(self.air_density*self.Cd*self.surface))
     def deplace(self):
-        self.rect=self.rect.move(self.x_speed,self.speed)
+        v=self.speed
+        traine=0.5*self.air_density*self.Cd*self.surface*v**2/self.mass
+        a=self.g-traine
+        self.speed+=a*self.dt
+        y_velocity=self.speed/self.mps*self.dt
+        self.speed=min(self.speed,30)
+        self.speed+=self.gravity
+        self.rect=self.rect.move(self.x_speed,int(y_velocity))
+        angle=math.atan(self.x_speed/y_velocity)
+        current_frame=self.frames[self.frame_index]
+        self.frame=pygame.transform.rotate(current_frame,angle*180/math.pi)
+        centre=self.rect.center
+        self.rect=self.frame.get_rect(center=centre)
     def collision(self):
         return self.rect.colliderect(targetRect)
+    def impact_force(self):
+        return int(0.5*self.mass*(self.x_speed**2+self.speed/self.mps*self.dt)/self.point_of_fall)
     def load_frames(self,frame_width=448,frame_height=448):
         frames = []
         sheet_width = self.image.get_width()
@@ -29,7 +57,8 @@ class Meteor:
             scale_w=int(frame_width*self.size)
             scale_h=int(frame_height*self.size)
             frame=pygame.transform.scale(frame,(scale_w,scale_h))
-            frame=pygame.transform.rotate(frame,math.atan(self.x_speed/self.speed)*180/math.pi)
+            
             frames.append(frame)
         self.rect=frames[0].get_rect(center=self.rect.center)
         return frames
+
