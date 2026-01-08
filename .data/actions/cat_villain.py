2d60dd0cfd92206df614bf0b974f1929ccda9557
petertenfields-ui <peter.tenfields@gmail.com>
2026-01-08 20:56:59 +0100

    Add cat villain event with dialogue and actions

---
+++ .data/actions/cat_villain.py
@@ -0,0 +1,63 @@
+{
+  "id":"cat",
+  "type":"event-positif",
+  "run":[
+    {
+      "type":"dialogue",
+      "lines":[
+        "Vous rencontrez un chat sur le chemin"
+      ]
+    },
+    {
+      "type":"select",
+      "question":"Que voulez-vous faire?",
+      "options":[
+        {
+          "name":"Le caresser",
+          "valeur":"pet"
+        },
+        {
+          "name":"Passer votre chemin",
+          "valeur":"vilain"
+        }
+      ],
+      "actions":{
+        "pet":[{
+          "type":"dialogue",
+          "lines":[
+            "Le chat ronronne et s'enroule autour de votre bras"
+          ]
+        },
+              {
+                "type":"dialogue",
+                "lines":[
+                  "Soudain, il profite de votre relâchement pour passer à l'attaque!",
+                  "Des cornes lui poussent, ses cornées deviennent rouge sang"
+                ]
+              },
+               {
+                 "type":"dialogue",
+                 "lines":[
+                   "Poussant un cri à faire glacer le sang au plus sang-chaud des bourrés après un match, il vous déchique la peau,",
+                   "vous retirant ongles et poils, mordant vos tendons, rendant inutilisables vos articulations et méconnaissable votre bras.",
+                   "avant de disparaître prestement, comme il était venu, sans un bruit.",
+                   "Vous avez eu quelques secondes de bonheur, mais ces choses-là ne durent jamais."
+                 ]
+               },
+              {
+                "type":"execution",
+                "code":"jeu.equipe.infliger(10)"
+              }],
+        "vilain":[
+          {
+            "type":"dialogue",
+            "lines":[
+              "Mieux vaut passer son chemin,",
+              "Plutôt que de se faire aguicher par le malin"
+            ]
+          }
+        ]
+      }
+    }
+  ]
+}
